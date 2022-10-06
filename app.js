fa26bd258fcad9c0cbce57dde4d6c22168999af2
Braxton <Bjett2009@gmail.com>
2022-10-05 22:13:53 -0400

    Es2015 oop

---
+++ app.js
@@ -0,0 +1,47 @@
+class Vehicle {
+	constructor(make, model, year) {
+		this.make = make;
+		this.model = model;
+		this.year = year;
+	}
+	honk() {
+		return "Beep";
+	}
+	toString() {
+		return `The vehicle is a ${make} ${model} made in ${year}.`;
+	}
+}
+
+class Car extends Vehicle {
+	constructor(make, model, year) {
+		super(make, model, year);
+		this.numWheels = 4;
+	}
+}
+
+class Motorcycle extends Vehicle {
+	constructor(make, model, year) {
+		super(make, model, year);
+		this.numWheels = 2;
+	}
+	revEngine() {
+		return "Vroom!";
+	}
+}
+
+class Garage {
+	constructor(capacity) {
+		this.vehicles = [];
+		this.capacity = capacity;
+	}
+	add(newVehicle) {
+		if (!(newVehicle instanceof Vehicle)) {
+			return "Only vehicles are allowed!";
+		}
+		if (this.vehicles.length >= this.capacity) {
+			return "We're full.";
+		}
+		this.vehicles.push(newVehicle);
+		return "Vehicle added!";
+	}
+}
